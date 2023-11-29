bb8971755e218edc80b84c342e597c5c1a9f680c
Bruno Giuseppe <brunogiuseppeh@gmail.com>
2023-11-29 15:38:11 -0300

    updateQuiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,75 +1,93 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from "react";
 
-class Quiz extends Component {
+const Quiz = () => {
+  const [perguntas, setPerguntas] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [score, setScore] = useState(null);
+  const [quizCompleted, setQuizCompleted] = useState(false);
 
+  const url = "http://localhost:3000/perguntas";
 
-  handleOptionSelect = (questionIndex, selectedOption) => {
-    this.setState((prevState) => ({
-      selectedOptions: {
-        ...prevState.selectedOptions,
-        [questionIndex]: selectedOption,
-      },
-    }));
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (Array.isArray(data) && data.length > 0 && "pergunta" in data[0]) {
+          setPerguntas(data);
+          setSelectedOptions(Array(data.length).fill(null));
+        } else {
+          console.error('Formato de resposta inválido:', data);
+        }
+      } catch (error) {
+        console.error("Erro ao buscar perguntas:", error);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  const handleOptionSelect = (questionIndex, selectedOption) => {
+    setSelectedOptions((prevSelectedOptions) => {
+      const newSelectedOptions = [...prevSelectedOptions];
+      newSelectedOptions[questionIndex] = selectedOption;
+      return newSelectedOptions;
+    });
   };
 
-  handleSubmitQuiz = () => {
-    const { questions, selectedOptions } = this.state;
-    
-    const allQuestionsAnswered = questions.every((question, index) => {
-      return selectedOptions[index] !== undefined;
-    });
+  const handleSubmitQuiz = () => {
+    const allQuestionsAnswered = selectedOptions.every(
+      (selectedOption) => selectedOption !== null
+    );
 
-    if (allQuestionsAnswered){
-      let score = 1;
-      questions.forEach((question, index)=> {
-        if (selectedOptions[index] === question.correctAnswer){
-          score++
+    if (allQuestionsAnswered) {
+      let score = 0;
+      perguntas.forEach((question, index) => {
+        if (selectedOptions[index] === question.respostaCorreta) {
+          score++;
         }
       });
-    
-    this.setState({ score, quizCompleted: true });
-  } else{
-    alert('Responda todas as perguntas antes de enviar o Quiz.');
-  }
+
+      setScore(score);
+      setQuizCompleted(true);
+    } else {
+      alert("Responda todas as perguntas antes de enviar o Quiz.");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Quiz ReactJS</h2>
+      {perguntas.map((question, index) => (
+        <div key={index}>
+          <h3>{question.pergunta}</h3>
+          <ul>
+            {Object.entries(question.alternativas).map(([opcao, textoOpcao]) => (
+              <li key={opcao}>
+                <label>
+                  <input
+                    type="radio"
+                    value={opcao}
+                    checked={selectedOptions[index] === opcao}
+                    onChange={() => handleOptionSelect(index, opcao)}
+                  />
+                  {`${opcao}: ${textoOpcao}`}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+      <button onClick={handleSubmitQuiz}>Enviar Quiz</button>
+      {quizCompleted && score !== null && (
+        <div>
+          <h2>Quiz completado!</h2>
+          <p>Sua pontuação: {score} de {perguntas.length}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
-  render() {
-    const { questions, selectedOptions, score, quizCompleted } = this.state;
-
-    return (
-      <div>
-        <h2>Quiz ReactJS</h2>
-        {questions.map((question, index) => (
-          <div key={index}>
-            <h3>Questão {index + 1}</h3>
-            <p>{question.question}</p>
-            <ul>
-              {question.options.map((option, optionIndex) => (
-                <li key={optionIndex}>
-                  <label>
-                    <input
-                      type="radio"
-                      value={option}
-                      checked={selectedOptions[index] === option}
-                      onChange={() => this.handleOptionSelect(index, option)}
-                    />
-                    {option}
-                  </label>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-        <button onClick={this.handleSubmitQuiz}>Enviar Quiz</button>
-        {quizCompleted && score !== null && (
-          <div>
-            <h2>Quiz completado!</h2>
-            <p>Sua pontuação: {score} de {questions.length}</p>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
-
-export default Quiz; 
+export default Quiz;
